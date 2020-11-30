ee47248ebf6699c75327f39a0d35a17ff490d0b0
LeonardoSaenz <leonardorsaenz@gmail.com>
2020-11-30 10:25:59 -0600

    Ultima Versión

---
+++ Deku/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Deku.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Deku.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Category> Get()
+        {
+            using (var context = new NorthwindContext())
+            {
+                return context.Categories.ToList();
+            }
+        }
+    }
+}
